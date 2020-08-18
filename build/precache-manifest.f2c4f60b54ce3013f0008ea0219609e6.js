54b40ccb083f518a808ef00b85998ec45f3b5f80
alexander <alsoderg@student.jyu.fi>
2020-08-18 17:32:37 +0300

    3.11 melkein

---
+++ build/precache-manifest.f2c4f60b54ce3013f0008ea0219609e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3766cdc0292a03748efc25505d9a30",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec315cb1b6d5d55d092d",
+    "url": "/static/css/main.d2910d7f.chunk.css"
+  },
+  {
+    "revision": "b83d5d237c05fa753d51",
+    "url": "/static/js/2.4d51bd9c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4d51bd9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec315cb1b6d5d55d092d",
+    "url": "/static/js/main.006a44a1.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
